4aa017ea115859509b6b76554d277382038f4204
Ricardo Rodríguez <ricardo.rodriguez@swiss4ward.com>
2018-11-05 16:55:31 +0100

    Add error handler

--- api/forum.js
+++ api/forum.js
@@ -1,59 +1,113 @@
 var ensureAuthenticated = require('../middleware/auth').ensureAuthenticated;
 app.get('/forums', function(req, resp){
-    getForums(function (data){
+    getForums(function (data, err){
+        if(err){
+            resp.status(500).send({error: "Unable to retrieve forums"});
+        }else{
         resp.status(200).send(data);
+        }
     })
 
 
 })
 app.get('/forums/:id', function(req, resp){
-    getForumById(req.params.id,function (data){
+    getForumById(req.params.id,function (data, err){
+        if(err){
+            resp.status(400).send({error: "Unable to retrieve forum with id: " + req.params.id});
+        }else{
         resp.status(200).send(data);
+        }
     })
 })
 
 app.delete('/forums/:id', ensureAuthenticated, function(req, resp){
-    deleteForum(req.params.id, function (data){
-        resp.status(200).send({message: "Deleted forum with id: " + req.params.id});
+    deleteForum(req.params.id, function (data, err){
+        if(err){
+            resp.status(400).send({error: "Unable to delete forum with id: " + req.params.id});
+        }else{
+        resp.status(200).send({error: "Deleted forum with id: " + req.params.id});
+        }
     })
 
 })
 
 app.post('/forums',ensureAuthenticated, function(req, resp){
-    var name = req.body.name
+    var name = req.body.name;
     if(name){
-        createForum(name,function(data){
+        createForum(name,function(data, err){
+            if(err){
+                resp.status(400).send({error: "Unable to create forum"});
+            }else{
             resp.status(201).send(data);
+            }
         })
     }
     else{
-        resp.status(400).send("Missing name");
+        resp.status(400).send({error :"Missing name"});
     }
+})
+
+app.put('/forums/:id', ensureAuthenticated, function(req, resp){
+    var name = req.body.name;
+    if(name){
+        updateForum(req.params.id, name, function(data, err) {
+            if(err){
+                resp.status(400).send({error: "Unable to update forum with id: " + req.params.id});
+            }else{
+            resp.status(200).send("Updated forum with id: " + req.params.id);
+            }
+        })
+    }
+
+
 })
 function getForums(callback){
     knex.select().from('Forum').
     then(function (data){
-        callback(data);
-    })
+        callback(data, undefined);
+    }).
+    catch(function (error){
+        callback(undefined, error);
+     });
 }
 
 function getForumById(id, callback){
     knex.select('name').from('Forum').where('id', id).
     then(function (data){
-        callback(data);
-    })
+        callback(data, undefined);
+    }).
+    catch(function (error){
+        callback(undefined, error);
+     });
 }
 
 function createForum(name, callback){
     knex('Forum').insert({name: name},'id').
     then(function (data){
-        callback(data);
-    })
+        callback(data, undefined);
+    }).
+    catch(function (error){
+        callback(undefined, error);
+     });
 }
 
 function deleteForum(id, callback){
     knex('Forum').where('id', id).del().
     then(function (data){
-        callback(data);
-    })
+        callback(data, undefined);
+    }).
+    catch(function (error){
+        callback(undefined, error);
+     });;
+}
+
+function updateForum(id, data, callback,err){
+    knex('Forum').where('id',id).update({
+        name: data.name
+    }).then(function (data){
+        callback(data, undefined);
+    }).
+    catch(function (error){
+       callback(undefined, error);
+    });
 }
